90e99d4abd124f1dda548df8829178ddbbdce5f5
ashworks1706 <asriv132@asu.edu>
2026-02-08 14:36:57 -0700

    added manifest

---
+++ sdk/pip/piramid/__init__.py
@@ -0,0 +1,7 @@
+"""Piramid SDK - Vector database for agentic applications."""
+
+__version__ = "0.1.0"
+
+def hello():
+    """Minimal placeholder function."""
+    return "Piramid vector database SDK"
