5a163d3a61b8ddc2aba787933d51241e0157fe52
ashworks1706 <asriv132@asu.edu>
2026-05-23 18:26:20 -0700

    0.2.0 pre-release

--- benches/hnsw_performance.rs
+++ benches/hnsw_performance.rs
@@ -9,7 +9,8 @@ fn hnsw_insert_search_bench(c: &mut Criterion) {
     let mut vectors = HashMap::new();
     let metadatas: HashMap<Uuid, Metadata> = HashMap::new();
 
-    for i in 0..1_0000 {
+    // Seed a small set of vectors
+    for i in 0..1_000 {
         let id = Uuid::new_v4();
         let vec = vec![i as f32, (i * 2) as f32, (i * 3) as f32];
         vectors.insert(id, vec.clone());
@@ -19,7 +20,7 @@ fn hnsw_insert_search_bench(c: &mut Criterion) {
 
     let query = vec![10.0, 20.0, 30.0];
 
-    c.bench_function("hnsw_search_10k", |b| {
+    c.bench_function("hnsw_search_1k", |b| {
         b.iter(|| {
             let reader = HashMapVectorReader::new(&vectors);
             let _ = index.search(&query, 10, 200, &reader, None, &metadatas);
